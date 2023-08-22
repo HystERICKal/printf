367e9e7424f5da9afc171651bb5e16ba8af65a8d
HystERICKal <nyoroerick12@gmail.com>
2023-08-22 16:51:55 -0700

    Error Fixing

--- custom_print_f.c
+++ custom_print_f.c
@@ -6,66 +6,52 @@
  */
 int _printf(const char *format, ...)
 {
-	int count_char = 0;         /*counts the characters printed*/
-	va_list variable_args_list; /*declare pointer to the arg list*/
+	int count_char = 0;
+	va_list args;
 
-	if (format == NULL)         /*check if it exists*/
-		return (-1);
-	va_start(variable_args_list, format);        /*initialise list ptr argument*/
+	va_start(args, format);
 	while (*format != '\0')
 	{
-		if (*format != '%') /*if there's no format specifier*/
+		if (*format == '%')
 		{
-			write(1, format, 1); /*Write to stdout*/
-			count_char++;        /*Increment the character count*/
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					{
+						char c = va_arg(args, int);
+
+						write(1, &c, 1);
+						count_char++;
+					}
+					break;
+				case 's':
+					{
+					char *str = va_arg(args, char *);
+
+					while (*str != '\0')
+					{
+						write(1, str, 1);
+						str++;
+						count_char++;
+					}
+					}
+					break;
+				case '%':
+					write(1, "%", 1);
+					count_char++;
+					break;
+				default:
+					break;
+			}
 		}
 		else
 		{
-			format++;            /*Check the next character after % sign*/
-			if (*format == '\0') /*End of sentence (NULL terminator)*/
-				break;
-
-			format_spec(variable_args_list, format);
+			write(1, format, 1);
 			count_char++;
-
-			if (*format == 's')
-			{
-				int LOW = str_dealer(variable_args_list);
-
-				count_char = count_char + LOW;
-			}
 		}
 		format++;
 	}
-	va_end(variable_args_list);
+	va_end(args);
 	return (count_char);
-}
-
-/**
- * format_spec - checks what format specifier is passed in
- * @spec_list: Declare pointer to arg list
- * @format_specifier: "c,d,i,%"
- * Return: nothing
- */
-void format_spec(va_list spec_list, const char *format_specifier)
-{
-	if (*format_specifier == '%')
-		perc_dealer(format_specifier);
-
-	else if (*format_specifier == 'c')
-		char_dealer(spec_list);
-
-	else if (*format_specifier == 'd' || *format_specifier == 'i')
-		d_and_i_dealer(spec_list);
-
-	else if (*format_specifier == 'b')
-		bin_dealer(spec_list);
-
-	else if (*format_specifier == 'u')
-		unsigned_dealer(spec_list);
-
-	else if (*format_specifier == 'o')
-		oct_dealer(spec_list);
-	else if (*format_specifier == 'x' || *format_specifier == 'X')
-		hex_dealer(spec_list, format_specifier);
 }
